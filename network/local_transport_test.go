fcf099132bc1ba33d59913e40b566d916d1a587c
ayushn2 <iayush.n2@gmail.com>
2025-05-05 13:58:17 +0530

    Add test for message validation in peer communication

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -13,3 +13,23 @@ func TestConnect(t *testing.T) {
 	assert.Equal(t, tra.peers[trb.addr], trb)
 	assert.Equal(t, trb.peers[tra.addr], tra)
 }
+
+func TestSendMessage(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+
+	tra.Connect(trb)
+	trb.Connect(tra)
+
+	msg := []byte("Hello, World!")
+	err := tra.SendMessage(trb.addr, msg)
+	assert.NoError(t, err)
+
+	select {
+	case rpc := <-trb.Consume():
+		assert.Equal(t, rpc.From, tra.addr)
+		assert.Equal(t, rpc.Payload, msg)
+	default:
+		t.Error("Expected message not received")
+	}
+}
