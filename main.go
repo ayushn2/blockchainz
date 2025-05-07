e05553389b24eab4dd82fb24eee1cda5d42fdc6f
ayushn2 <iayush.n2@gmail.com>
2025-05-07 17:41:00 +0530

    feat: implement basic server with transport init, RPC handling, and graceful shutdown

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/ayushn2/blockchainz/network"
+)
 
 // Server
 // Transport => tcp, udp
@@ -8,5 +12,25 @@ import "fmt"
 // TX
 
 func main() {
-	fmt.Println("Hello, World!")
+	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+	// Send a message from LOCAL to REMOTE
+
+	go func(){
+		for {
+			// Simulate sending messages
+			// local will send message to remote every second in a seperate go routine
+			trRemote.SendMessage(trLocal.Addr(), []byte("Hello from REMOTE"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trLocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
 }
--- network/server.go
+++ network/server.go
@@ -1,9 +1,58 @@
 package network
 
+import (
+	"fmt"
+	"time"
+)
+
 type ServerOpts struct{
 	Transports []Transport
 }
 
 type Server struct {
+	ServerOpts
+	rpcCh chan RPC
+	quitch chan struct{}
+}
 
+func NewServer(opts ServerOpts) *Server {
+	return &Server{
+		ServerOpts: opts,
+		rpcCh: make(chan RPC),
+		quitch: make(chan struct{}, 1),
+	}
+}
+
+func (s *Server) Start() {
+	s.initTransports()
+	ticker := time.NewTicker(5 * time.Second)
+
+free:
+	for {
+		select {
+		case rpc := <-s.rpcCh:
+			// Handle incoming RPC
+			fmt.Printf("Received RPC from %s: %s\n", rpc.From, string(rpc.Payload))
+		case <-s.quitch:
+			break free
+		case <-ticker.C:
+			fmt.Println("Server is running...")
+		}
+	}
+
+	fmt.Println("Server shutting down...")
+}
+
+func (s *Server) initTransports() {
+	for _, tr := range s.Transports {
+		go func(tr Transport) {
+			for rpc := range tr.Consume() {//keep consuming the message channels
+				// Handle incoming RPC
+				// For example, you might want to process the message or forward it
+				// to another transport
+				fmt.Printf("Received message from %s: %s\n", rpc.From, string(rpc.Payload))
+				s.rpcCh <- rpc // Forward the RPC to the server's main channel
+			}
+		}(tr)
+	}
 }
